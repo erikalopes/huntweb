554d463c80544a0b93382353a3fb6c068353da78
erikacalu <201810099@aluno.fjn.edu.br>
2019-09-01 00:45:17 -0300

    added list view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import api from './services/api';
 import Header from './components/Header';
 import "./styles.css";
-
+import Main from './pages/main'
 const App = () => ( 
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
