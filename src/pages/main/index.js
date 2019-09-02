1706bfb805f680d5fe54bd0ca5a10d4506a3f55f
erikacalu <201810099@aluno.fjn.edu.br>
2019-09-02 00:31:28 -0300

    nextPage and prevPage added

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,20 +5,41 @@ import "./styles.css";
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        this.setState({ products: response.data.docs })
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const { docs, ...productInfo } = response.data;
+        this.setState({ products: docs, productInfo, page });
         // console.log(response.data.docs);
     }
 
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) return;
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+
+    }
     render() {
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
         return (
             <div className="product-list">
                 {products.map(product => (
@@ -28,10 +49,14 @@ export default class Main extends Component {
                         </strong>
                         <p>
                             {product.description}
-                            <a href=""> Acessar </a>
+                            <a> Acessar </a>
                         </p>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>proximo</button>
+                </div>
             </div>
         )
     }
